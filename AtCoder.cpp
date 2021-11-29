ec6b0d121870787fa283282fbc6042997938fafe
Syohei358 <syohei.yamazaki@cct-inc.co.jp>
2021-11-29 21:58:38 +0900

    20211129

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -396,3 +396,71 @@ int main() {
     // 結果出力
     cout << min_value << endl;
 }
+
+
+AtCoder Beginner Contest 140
+
+C - Maximal Value
+
+#include <bits/stdc++.h>
+#define rep(i,cc,n) for(int i=cc;i<=n;++i)
+#define drep(i,cc,n) for(int i=cc;i>=n;--i)
+#define sz(s) (int)(s.size())
+#define mod 1000000007
+using namespace std;
+int main(){
+  //入力を受け取る
+  int n;
+  cin >> n;
+  int b[n-1];
+  rep(i,0,n-2)cin >> b[i];
+  long long ans = b[0];
+  rep(i,1,n-2){
+    //b_i, b_{i+1}のうち小さい方がA_iの最大値となる
+    ans += min(b[i-1],b[i]);
+  }
+  ans += b[n-2];
+  cout << ans << endl;
+  return 0;
+}
+
+
+AtCoder Beginner Contest 135
+
+C - City Savers
+
+#include <bits/stdc++.h>
+#define rep(i,cc,n) for(int i=cc;i<=n;++i) 
+using namespace std;
+int main(){
+  //入力を受け取る
+  int n;
+  cin >> n;
+  long a[n+1],b[n],ans=0.0;
+  rep(i,0,n)cin >> a[i]; 
+  rep(i,0,n-1)cin >> b[i];
+  rep(i,0,n-1){
+    //勇者iが倒せる数が残っていないときは次の勇者へ
+    if(b[i]==0)continue;
+    //勇者iが倒せる数が残っているモンスターの数より少ないときは倒せるだけ倒す
+    //都市iのモンスターを倒しきれないとき
+    if(b[i] <= a[i]){
+      ans += b[i];
+      continue;
+    }
+    //勇者iが倒せる数が残っているモンスターの数より少ないときは倒せるだけ倒す
+    //都市iのモンスターは倒せるが都市i+1のモンスターは倒しきれないとき
+    if(b[i] <= a[i] + a[i+1]){
+      ans += b[i];
+      b[i] -= a[i];
+      a[i+1] -= b[i];
+      continue;
+    }
+    //勇者iが都市i, i+1のモンスターをすべて倒しきれるときはすべて倒す
+    ans += a[i]+a[i+1];
+    //a[i]も0にすべきだが、この変数はこの後使わないので省略
+    a[i+1]=0;
+  }
+  cout << ans << endl;
+  return 0;
+}
