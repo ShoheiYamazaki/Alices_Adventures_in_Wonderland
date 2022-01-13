d8458488e8d2af891baeb0daf2aba9851f98a0db
ShoheiYamazaki <shoheiyamazaki0101@gmail.com>
2022-01-13 23:33:25 +0900

    20220113

--- AtCoder.py
+++ AtCoder.py
@@ -1280,3 +1280,17 @@ if A == B:
     print("Yes")
 else:
     print("No")
+
+
+AtCoder Beginner Contest 201
+
+A - Tiny Arithmetic Sequence
+
+A = list(map(int, input().split()))
+A.sort()
+if A[0] == A[1] == A[2]:
+    print("Yes")
+elif A[1] - A[0] == A[2] - A[1]:
+    print("Yes")
+else:
+    print("No")
