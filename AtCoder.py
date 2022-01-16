37685c07a9b569d03bd78bcf26ce7c06108ddbac
ShoheiYamazaki <shoheiyamazaki0101@gmail.com>
2022-01-16 21:28:41 +0900

    20220116

--- AtCoder.py
+++ AtCoder.py
@@ -1373,3 +1373,16 @@ for i in range(Q):
                 break
     if int(ans) == 0:
         print(-1)
+
+from collections import defaultdict
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+m = defaultdict(list)
+for i in range(N):
+  m[A[i]].append(i + 1) #連想配列（辞書）を作成
+for _ in range(Q):
+  x, k = map(int, input().split())
+  if k <= len(m[x]):
+    print(m[x][k - 1])
+  else:
+    print(-1)
