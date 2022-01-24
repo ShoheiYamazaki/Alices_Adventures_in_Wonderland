a234f4483da103bc12cc8abc5f770e884b960e2c
ShoheiYamazaki <shoheiyamazaki0101@gmail.com>
2022-01-24 14:06:44 +0900

    20220124

--- AtCoder.py
+++ AtCoder.py
@@ -1496,3 +1496,9 @@ for i in range (len(S)):
             eki = True
     if eki == False:
         print("No")
+
+n, m = map(int, input().split())
+s = input().split()
+t = set(input().split())
+for x in s:
+    print("Yes" if x in t else "No")
