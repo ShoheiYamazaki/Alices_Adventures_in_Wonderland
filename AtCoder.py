24da4f5d03deda8b7b7b26f1ea8ac64a91b4eeab
ShoheiYamazaki <shoheiyamazaki0101@gmail.com>
2022-02-14 20:51:28 +0900

    20220214

--- AtCoder.py
+++ AtCoder.py
@@ -1694,3 +1694,14 @@ if x == 0:
     print(1)
 else:
     print(0)
+
+
+AtCoder Beginner Contest 174
+
+A - Air Conditioner
+
+X = int(input())
+if X >= 30:
+    print("Yes")
+else:
+    print("No")
