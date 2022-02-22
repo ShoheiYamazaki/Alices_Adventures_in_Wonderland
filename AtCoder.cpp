a2bc90cbb1696df6e26e019cd0db40c97ded4289
ShoheiYamazaki <shoheiyamazaki0101@gmail.com>
2022-02-22 22:44:28 +0900

    20220222

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -1262,3 +1262,37 @@ int main() {
     }
     cout << ans << endl;
 }
+
+
+AtCoder Beginner Contest 238
+
+C - digitnum
+//わっかんね。泣きそう。
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using ll = long long;
+using mint = modint998244353;
+
+mint sum(ll x) {
+  return mint(1+x)*x/2;
+}
+
+int main() {
+  ll n;
+  cin >> n;
+  ll l = 1;
+  int keta = 1;
+  mint ans;
+  while (l <= n) {
+    ll r = l*10;
+    // [l,r)
+    ll len = min(n+1,r)-l;
+    ans += sum(len);
+    l *= 10; keta++;
+  }
+  cout << ans.val() << endl;
+  return 0;
+}
