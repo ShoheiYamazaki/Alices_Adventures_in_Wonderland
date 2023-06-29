708e9156e526e05b996da04b3ec3e743a37e4eb4
ShoheiYamazaki <shoheiyamazaki0101@gmail.com>
2023-06-29 20:40:53 +0900

    20230629

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -1837,3 +1837,35 @@ int main() {
     }
     cout << "Yes" << endl;
 }
+
+
+AtCoder Beginner Contest 302
+
+C - Almost Equal
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<string> s(n);
+    rep(i,n) cin >>s[i];
+
+    sort(s.begin(), s.end());
+    do {
+        bool ok = true;
+        rep(i,n-1) {
+            int d = 0;
+            rep(j,m) if (s[i][j] != s[i+1][j]) d++;
+            if (d != 1) ok = false;
+        }
+        if (ok) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    } while (next_permutation(s.begin(), s.end()));
+    cout << "No" << endl;
+    return 0;
+}
